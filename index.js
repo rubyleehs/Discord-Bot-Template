856722f094e1dc8f7f95548ad294fbfa7f8c08c1
rubyleehs <rubyleehs.dev@gmail.com>
2020-08-08 16:09:45 +0800

    Base for Bot

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const botconfig = require(`./bot_config`);
+const fs = require('fs');
+
+fs.readdir('./events/', (err, files) =>
+{
+    files.forEach(file => 
+    {
+        const eventHandler = require(`./events/${file}`) //eventHandler refrences the other files/scripts/class
+        const eventName = file.split('.')[0]  //file name without extension
+        client.on(eventName, function (...arg) { eventHandler(client, ...arg) })
+        //client.on(name, arguments) creates the event with eventName
+        //eventName becomes an event which calls eventHandler function with the input (client,args)
+        //where client is clienet and ...args is any amount of input by user
+    })
+})
+
+client.login(botconfig.token);
